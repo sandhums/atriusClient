ad0cf5e8b403aa93d0dd392f964e5ff42971250d
Manjinder Sandhu <sandhums@gmail.com>
2023-10-07 17:22:05 +0530

    Initial Commit

---
+++ atriusClient/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  atriusClient
+//
+//  Created by Manjinder Sandhu on 07/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ atriusClient/atriusClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  atriusClientApp.swift
+//  atriusClient
+//
+//  Created by Manjinder Sandhu on 07/10/23.
+//
+
+import SwiftUI
+
+@main
+struct atriusClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
